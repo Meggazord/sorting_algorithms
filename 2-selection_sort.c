7917aa47fad4efd7a87b743e13d68b1ba9a30c71
Eric <ericgeorge4477@gmail.com>
2024-01-27 23:27:06 +0200

    selection sort

---
+++ 2-selection_sort.c
@@ -0,0 +1,27 @@
+#include <stddef.h>
+
+void swap(int a, intb) {
+    int temp = *a;
+    a =b;
+    b = temp;
+}
+
+void selection_sort(intarray, size_t size) {
+    if (array == NULL || size <= 1) {
+        return; // No need to sort an array of size 0 or 1
+    }
+
+    for (size_t i = 0; i < size - 1; ++i) {
+        size_t min_index = i;
+
+        for (size_t j = i + 1; j < size; ++j) {
+            if (array[j] < array[min_index]) {
+                min_index = j;
+            }
+        }
+
+        if (min_index != i) {
+            swap(&array[i], &array[min_index]);
+        }
+    }
+}
